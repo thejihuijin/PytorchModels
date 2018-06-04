fbbc0f7398bd04788427fd3eebda171e2dc37393
Jihui Jin <thejihuijin@gmail.com>
2018-06-04 18:09:41 -0400

    add train GANs to net_utils

--- net_utils.py
+++ net_utils.py
@@ -9,6 +9,7 @@ import torch.optim as optim
 import datetime
 import os
 import time
+import numpy as np
 
 def get_datetime():
     time = datetime.datetime.today()
@@ -16,6 +17,19 @@ def get_datetime():
     out = out+'-'+str(time.minute).zfill(2)
     out = out+'-'+str(time.second).zfill(2)
     return out
+def preprocess(data):
+    return torch.Tensor(data).unsqueeze(1)
+def target_ones(N,GPU=False):
+    if GPU:
+        return torch.ones(N,1).cuda()
+    else:
+        return torch.ones(N,1)
+
+def target_zeros(N,GPU=False):
+    if GPU:
+        return torch.zeros(N,1).cuda()
+    else:
+        return torch.zeros(N,1)  
 
 def train_net(net, trainloader, num_epochs, GPU=False, 
               weightpath='./weights/',save_epoch=50,
@@ -97,3 +111,145 @@ def train_net(net, trainloader, num_epochs, GPU=False,
 
     print('Finished Training')
 
+
+
+def train_GANs(G, D, faketrainloader, realtrainloader, num_epochs=500, GPU=False,
+              weightpath='./weights/',save_epoch=50,saveweights=True):
+    # Create output directory
+    weightpath = os.path.join(weightpath,get_datetime())
+    os.makedirs(weightpath)
+    logpath = os.path.join(weightpath,'log.txt')
+    
+    with open(logpath, "wt") as text_file:
+        print('Epoch\tD Loss\tG Loss\tEpoch Time\tTotal Time',file=text_file)
+
+    num_data = len(realtrainloader)*realtrainloader.batch_size 
+    d_losses = np.zeros(num_epochs)
+    g_losses = np.zeros(num_epochs)
+
+    # Accumulate log text
+    logtxt = ''
+    
+    # Determine minibatch size
+    minibatch = max(1,int(len(realtrainloader))/10)
+    
+    # Define Loss Function/Optimizer
+    bceloss = nn.BCELoss()
+    mseloss = nn.MSELoss()
+
+    d_optimizer = optim.Adam(D.parameters(), lr=0.0002)
+    g_optimizer = optim.Adam(G.parameters(), lr=0.0002)
+
+    
+    G.train()
+    trainstart = time.time()
+    for epoch in range(num_epochs):
+        # Collect loss information
+        d_epoch_loss = 0.0
+        g_epoch_loss = 0.0
+        d_running_loss = 0.0
+        g_running_loss = 0.0
+        
+        epochstart = time.time()
+
+        fakeiter = iter(faketrainloader)
+        realiter = iter(realtrainloader)
+        Giter = iter(faketrainloader)
+        for batch_index in range(len(realtrainloader)):
+            ## prepare data
+            truelabels = realiter.next()
+            fakeinput, _ = fakeiter.next()
+            batch_size = truelabels.size(0)
+
+
+            if GPU:
+                truelabels = truelabels.cuda()
+                fakeinput = fakeinput.cuda()
+    #             fakelabel = fakelabel.cuda()
+            d_real_data = Variable(truelabels)
+            d_gen_input = Variable(fakeinput)
+            d_fake_data = G(d_gen_input).detach() # detach to avoid training G on these labels
+            
+            ## Train D
+            d_optimizer.zero_grad()
+
+            # Train D on real
+            d_real_decision = D(d_real_data)
+            d_real_error = bceloss(d_real_decision, Variable(target_ones(batch_size,GPU)))
+            d_real_error.backward()
+
+            # Train D on fake
+
+            d_fake_decision = D(d_fake_data)
+            d_fake_error = bceloss(d_fake_decision, Variable(target_zeros(batch_size,GPU))) 
+            d_fake_error.backward()
+            d_optimizer.step()
+            d_loss = d_real_error+d_fake_error
+            
+            d_running_loss += d_loss.data[0]
+            d_losses[epoch] += d_loss.data[0]
+            
+        
+            ## Train G
+            g_fake_input, g_fake_label = Giter.next()
+            batch_size = g_fake_input.size(0)
+
+            if GPU:
+                g_fake_input = g_fake_input.cuda()
+                g_fake_label = g_fake_label.cuda()
+
+            gen_input = Variable(g_fake_input)
+            g_fake_data = G(gen_input)
+  
+            g_optimizer.zero_grad()
+
+            dg_fake_decision = D(g_fake_data)
+            g_loss = (10**-3)*bceloss(dg_fake_decision, Variable(target_ones(batch_size,GPU)))
+            g_loss +=  mseloss(g_fake_data,Variable(g_fake_label))
+
+            g_loss.backward()
+            g_optimizer.step()
+            
+            g_running_loss += g_loss.data[0]
+            g_losses[epoch] += g_loss.data[0]
+            
+            # print statistics
+            if batch_index % minibatch == 0:
+                print('\t[%d, %5d] D loss: %.3f, G loss: %.3f, %.3f seconds elapsed' %
+                      (epoch + 1, batch_index + 1, d_running_loss / minibatch, 
+                       g_running_loss/minibatch, time.time() - epochstart))
+                d_running_loss = 0.0
+                g_running_loss = 0.0
+        # Record epoch statistics
+        epochend = time.time()        
+        print('Epoch %d Training Time: %.3f seconds\nTotal Elapsed Time: %.3f seconds' %
+               (epoch+1, epochend-epochstart,epochend-trainstart))
+        
+        # log losses
+        d_losses[epoch] /= num_data
+        g_losses[epoch] /= num_data
+        logtxt += '%i\t%f\%f\t%f\t%f\n' % (epoch+1,d_losses[epoch], g_losses[epoch],
+                                           epochend-epochstart,epochend-trainstart)
+
+        
+        # Save weights
+        if (epoch % save_epoch == 0 or epoch == num_epochs-1):
+            if saveweights:
+                d_outpath = os.path.join(weightpath,'D_epoch_'+str(epoch+1)+'.weights')
+                g_outpath = os.path.join(weightpath,'G_epoch_'+str(epoch+1)+'.weights')
+                D = D.cpu()
+                G = G.cpu()
+                torch.save(D.state_dict(),d_outpath)
+                torch.save(G.state_dict(),g_outpath)
+
+                if GPU:
+                    D = D.cuda()
+                    G = G.cuda()
+            
+            # write loss to logfile
+            with open(logpath, "at") as text_file:
+                print(logtxt[:-2],file=text_file)
+                logtxt = ''
+
+    print('Finished Training')
+    return d_losses,g_losses
